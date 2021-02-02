b011cfa06abb4d14f6e7d594d37d4a7063a3a065
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-02 08:48:23 +0330

    Creating root and label widgets

--- main/number_02.py
+++ main/number_02.py
@@ -2,3 +2,10 @@
 
 # import necessary modules
 from tkinter import *
+
+# creating root widget
+root = Tk()
+
+# creating label widgets
+my_label_1 = Label(root, text="HelloWorld")
+my_label_2 = Label(root, text="My name is EverLookNeverSee")
