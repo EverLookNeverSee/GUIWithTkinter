4b17514b1d8eb7d2926ae4a6219eef7e217f0158
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-02 11:18:46 +0330

    do something whenever clicking on button

--- main/number_03.py
+++ main/number_03.py
@@ -6,8 +6,13 @@ from tkinter import *
 # Creating root widget
 root = Tk()
 
+def my_click():
+    my_label = Label(root, text="I clicked a button")
+    my_label.pack()
+
+
 # Creating button widget and packing it
-myButton = Button(root, text="Click on me!", padx=50, pady=50)
+myButton = Button(root, text="Click on me!", padx=50, pady=50, command=my_click)
 myButton.pack()
 
 root.mainloop()
