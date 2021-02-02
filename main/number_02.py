5ce6241f3ee92095c878ef1a356358c4c7259497
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-02 08:50:37 +0330

    setting grid positioning

--- main/number_02.py
+++ main/number_02.py
@@ -9,3 +9,11 @@ root = Tk()
 # creating label widgets
 my_label_1 = Label(root, text="HelloWorld")
 my_label_2 = Label(root, text="My name is EverLookNeverSee")
+
+
+# setting grid positioning system
+my_label_1.grid(row=0, column=0)
+my_label_2.grid(row=1, column=1)
+
+
+root.mainloop()
