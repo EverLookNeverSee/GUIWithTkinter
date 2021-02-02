086504ec8de01119afd58e04ef1e2765f6dde493
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-02 11:08:26 +0330

    Creating simple button

--- main/number_03.py
+++ main/number_03.py
@@ -2,3 +2,12 @@
 
 # importing relevant libraries
 from tkinter import *
+
+# Creating root widget
+root = Tk()
+
+# Creating button widget and packing it
+myButton = Button(root, text="Click on me!")
+myButton.pack()
+
+root.mainloop()
