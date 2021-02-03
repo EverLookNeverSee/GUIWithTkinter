8d0c9f3b7a46287f0f037e291347253dc6ff8c9e
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-03 11:05:54 +0330

    creating and positioning an entry

--- main/number_05.py
+++ main/number_05.py
@@ -7,3 +7,9 @@ from tkinter import *
 root = Tk()
 # setting app title
 root.title("Simple Calculator")
+
+# creating an entry and positioning using grid system
+e = Entry(root, width=35, borderwidth=3)
+e.grid(row=0, column=0, padx=10, pady=10)
+
+root.mainloop()
