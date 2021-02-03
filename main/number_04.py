173f7de6ac2083b8a2fd7a979a774b6872b13dc1
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-03 10:30:12 +0330

    greeting to user

--- main/number_04.py
+++ main/number_04.py
@@ -7,7 +7,14 @@ from tkinter import *
 root = Tk()
 
 # Creating an entry widget
-e = Entry(root, borderwidth=3)
+e = Entry(root, borderwidth=3, width=50)
 e.pack()
+
+def myClick():
+    my_label = Label(root, text=f"Hello {e.get()}")
+    my_label.pack()
+
+my_button = Button(root, text="Enter your name", command=myClick)
+my_button.pack()
 
 root.mainloop()
