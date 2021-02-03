9640c6b5c87d5959036588c892e0e50833391e7b
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-02-03 10:32:43 +0330

    putting text inside the box

--- main/number_04.py
+++ main/number_04.py
@@ -9,12 +9,13 @@ root = Tk()
 # Creating an entry widget
 e = Entry(root, borderwidth=3, width=50)
 e.pack()
+e.insert(0, "Enter your name")
 
 def myClick():
     my_label = Label(root, text=f"Hello {e.get()}")
     my_label.pack()
 
-my_button = Button(root, text="Enter your name", command=myClick)
+my_button = Button(root, text="Enter", command=myClick)
 my_button.pack()
 
 root.mainloop()
